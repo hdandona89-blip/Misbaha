23c545d27cba24c52e29515c4c7f507606eddfbf
hdandona89-blip <hdandona89@gmail.com>
2026-08-01 14:00:26 +0300

    Create ons / lib / models / zekr_model.dart

---
+++ ons / lib / models / zekr_model.dart
@@ -0,0 +1,11 @@
+class ZekrModel {
+  final String text;
+  final String count;
+  final String reference;
+
+  ZekrModel({
+    required this.text,
+    required this.count,
+    required this.reference,
+  });
+}
